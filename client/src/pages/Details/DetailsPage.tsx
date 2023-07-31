cd18d72431599cdcf7f237dec655c114f5dc2363
RhysDevalckeneer2 <44397726+RhysDevalckeneer2@users.noreply.github.com>
2023-07-31 17:03:06 +0200

    feat: changed dynamic detailpage

--- client/src/pages/Details/DetailsPage.tsx
+++ client/src/pages/Details/DetailsPage.tsx
@@ -1,30 +1,51 @@
+import { useEffect, useState } from 'react'
+import axios from "axios"
 import { FaClock, FaStar, FaHeartOutline } from 'react-icons/fa';
 import { Container } from '../../components';
+import { useParams } from 'react-router-dom';
 
 function DetailsPage () {
+
+    let { id } = useParams();
+
+    const [ movie, setMovie ] = useState();
+
+    const getMovieDetails = () => {
+        axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=ace96559b8fb0dd613fdfd48023afa84&language=en-US`)
+        .then((data) => {
+            setMovie(data?.data);
+        })
+        
+    }
+
+    useEffect(() => {
+        getMovieDetails();
+        console.log(movie)
+    }, [id]);
+
     return (
-        <div className="">
+        <div className="bg-slate-50">
             <Container>
                 <header className="w-full h-64 rounded bg-slate-100 mb-4">
-                    <img src="https://www.themoviedb.org/t/p/original/fm6KqXpk3M2HVveHwCrBSSBaO0V.jpg" className="h-full w-full object-cover rounded" />
+                    <img src={'https://www.themoviedb.org/t/p/original'+movie.backdrop_path} className="h-full w-full object-cover rounded" />
                 </header>
                 <div className="flex flex-row">
                     <div className="mr-6 w-2/12">
                         <img 
-                            src="https://www.themoviedb.org/t/p/w1280/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg" 
+                            src={'https://www.themoviedb.org/t/p/w1280'+movie.poster_path} 
                             className="rounded w-42"/>
                     </div>
-                    <div className="w-6/12">
+                    <div className="w-10/12">
                         <div className="flex flex-row align-center justify-start">
-                            <h1 className="font-bold text-3xl text-slate-700 font-serif mr-4">Oppenheimer</h1>
+                            <h1 className="font-bold text-3xl text-slate-700 font-serif mr-4">{movie.title}</h1>
                             <p className="text-lg text-slate-400 font-sans font-light mt-1">(2023)</p>
                         </div>
                         <div className="flex flex-row mt-4">
                             <p className="flex align-center content-center text-base justify-start mr-2 text-slate-600 mr-6">
-                                <FaClock className="mt-1 mr-2 text-blue-500"/>3u01
+                                <FaClock className="mt-1 mr-2 text-blue-500"/>{movie.runtime} min
                             </p>
                             <p className="flex align-center content-center text-base justify-start mr-2 text-slate-600 mr-4">
-                                <FaStar className="mt-1 mr-2 text-yellow-400"/>8.8
+                                <FaStar className="mt-1 mr-2 text-yellow-400"/>{movie.vote_average}
                             </p>
                         </div>
                         <div className="flex flex-row mt-4">
@@ -32,7 +53,7 @@ function DetailsPage () {
                             <p className="bg-slate-200 text-slate-800 py-1 px-2 rounded text-xs font-semibold mr-2">History</p>
                         </div>
                         <div className="mt-4">
-                            <p className="text-slate-400 font-light">The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.</p>
+                            <p className="text-slate-400 font-light">{movie.overview}</p>
                         </div>
                         <div className="my-4">
                             <h3 className="font-serif text-base text-slate-700 font-bold">Directed by</h3>
