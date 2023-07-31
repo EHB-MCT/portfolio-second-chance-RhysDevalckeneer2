1a880498cc304e530a5ead029f66a5b65ed3275e
RhysDevalckeneer2 <rhys.devalckeneer@hotmail.be>
2023-07-31 08:38:57 +0200

    feat: added search page component

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,4 +2,5 @@ export { default as DetailsPage } from "./Details/DetailsPage";
 export { default as FavoritePage } from "./Favorites/FavoritePage";
 export { default as HomePage } from "./Home/HomePage";
 export { default as NotFoundPage } from "./Error/NotFoundPage";
+export { default as SearchPage } from "./Search/SearchPage";
 export { default as WatchedPage } from "./Watched/WatchedPage";
