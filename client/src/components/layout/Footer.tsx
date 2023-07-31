ef46564d2cfc21c8a5ba6d43691f0405a47a8d20
RhysDevalckeneer2 <44397726+RhysDevalckeneer2@users.noreply.github.com>
2023-07-31 09:12:43 +0200

    feat: updated footer

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <Container>
                 <div className="grid grid-cols-2 gap-4">
                     <div className="flex content-center justify-start">
-                        <p className="text-xs text-slate-300">&copy;2023 M. All rigths reserved.</p>
+                        <p className="text-xs text-slate-300">&copy;2023 Movieclub. All rigths reserved.</p>
                     </div>
                     <div className="flex content-center justify-end">
                         <a href="#" className="text-xs text-slate-300 mr-6 underline underline-offset-2">Terms of Sevice</a>
