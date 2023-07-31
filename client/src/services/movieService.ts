360c65a8f8cdb90d98dac97ea601261f75ea11aa
RhysDevalckeneer2 <rhys.devalckeneer@hotmail.be>
2023-07-31 08:38:26 +0200

    refactor: added api service

---
+++ client/src/services/movieService.ts
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+
+const getMovieById = async () => {
+
+    if(!querry) return;
+    const { data } = await 
+        axios.fetch()
+}
+
+export default getMovieById;
