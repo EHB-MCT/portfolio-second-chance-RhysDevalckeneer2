d1ec3e914104361e8dfed79ef8b7d0cb407d7323
RhysDevalckeneer2 <44397726+RhysDevalckeneer2@users.noreply.github.com>
2023-07-31 09:13:01 +0200

    feat: updated header

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { Container } from '../index';
 import { Link } from "react-router-dom";
+import { FaEye, FaHeart, FaHome, FaSearch } from 'react-icons/fa';
 import logo from '../../assets/images/movieclub-logo.svg'
 
 const Header = () => {
@@ -8,22 +9,25 @@ const Header = () => {
             <Container>
                 <div className="grid grid-cols-2 gap-4">
                     <div className="flex content-center justify-start">
-                        {/* <img src={logo} alt="log MovieClub" className="w-20" /> */}
-                        logo
+                        <img src={logo} alt="log MovieClub" className="w-20" /> 
                     </div>
                     <nav className="flex content-center justify-end">
-                        <ul className="flex flex-row content-center justify-end list-none">
+                        <ul className="flex flex-row align-center content-center justify-end list-none mt-2">
                             <li>
-                                <Link to="/" className="flex flex-row content-center text-sm font-semibold text-slate-400 mr-10 hover:text-slate-500">Home</Link>
+                                <Link to="/" className="flex flex-row content-center text-sm font-semibold text-slate-400 mr-10 hover:text-slate-500">
+                                    <FaHome className="mt-[6px] mr-[6px] text-xs"/>Home</Link>
                             </li>
                             <li>
-                                <Link to="/search" className="flex flex-row content-center text-sm font-semibold text-slate-400 mr-10 hover:text-slate-500">Search</Link>
+                                <Link to="/search" className="flex flex-row content-center text-sm font-semibold text-slate-400 mr-10 hover:text-slate-500">
+                                    <FaSearch className="mt-[6px] mr-[6px] text-xs"/>Search</Link>
                             </li>
                             <li>
-                                <Link to="/watched" className="flex flex-row content-center text-sm font-semibold text-slate-400 mr-10 hover:text-slate-500"> Watched</Link>
+                                <Link to="/watched" className="flex flex-row content-center text-sm font-semibold text-slate-400 mr-10 hover:text-slate-500"> 
+                                    <FaEye className="mt-[6px] mr-[6px] text-xs"/>Watched</Link>
                             </li>
                             <li>
-                                <Link to="/favorites" className="flex flex-row content-center text-sm font-semibold text-slate-400 hover:text-slate-500">Favorites</Link>
+                                <Link to="/favorites" className="flex flex-row content-center text-sm font-semibold text-slate-400 hover:text-slate-500">
+                                    <FaHeart className="mt-[6px] mr-[6px] text-xs"/>Favorites</Link>
                             </li>
                         </ul>
                     </nav>
