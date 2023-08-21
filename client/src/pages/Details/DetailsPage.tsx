3a0aa95a38a722fefedab643b230fcba5b2a004b
RhysDevalckeneer2 <44397726+RhysDevalckeneer2@users.noreply.github.com>
2023-08-21 13:55:16 +0200

    feat: added loading state

--- client/src/pages/Details/DetailsPage.tsx
+++ client/src/pages/Details/DetailsPage.tsx
@@ -1,31 +1,46 @@
 import { useEffect, useState } from 'react'
 import axios from "axios"
-import { FaClock, FaStar, FaHeartOutline } from 'react-icons/fa';
+import { FaEye } from 'react-icons/fa';
 import { Container } from '../../components';
 import { useParams } from 'react-router-dom';
 
 function DetailsPage () {
 
     let { id } = useParams();
-
-    const [ movie, setMovie ] = useState();
+    const [ movie, setMovie ] :any = useState();
+    const [isLoading, setLoading] = useState(true)
 
     const getMovieDetails = () => {
         axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=ace96559b8fb0dd613fdfd48023afa84&language=en-US`)
         .then((data) => {
             setMovie(data?.data);
+            if(data && data.data) setLoading(false);
         })
-        
+        console.log(movie)
     }
 
-    useEffect(() => {
-        getMovieDetails();
-        console.log(movie)
-    }, [id]);
+    useEffect(getMovieDetails, [id]);
+
+    const addWatched = () => {
+        axios
+        .post(`http://localhost:6200/movies`, { 
+            movie_id: movie.id,
+            poster_path: movie.poster_path,
+            runtime: movie.runtime
+         })
+        .then((res) => {
+            console.log(res);
+            console.log(res.data);
+        });
+    }
 
     return (
-        <div className="bg-slate-50">
-            <Container>
+        <>
+        {isLoading ? (<p>Loading</p>) : (
+
+        <>
+            <div className="bg-slate-50">
+            <Container> 
                 <header className="w-full h-64 rounded bg-slate-100 mb-4">
                     <img src={'https://www.themoviedb.org/t/p/original'+movie.backdrop_path} className="h-full w-full object-cover rounded" />
                 </header>
@@ -41,24 +56,19 @@ function DetailsPage () {
                             <p className="text-lg text-slate-400 font-sans font-light mt-1">(2023)</p>
                         </div>
                         <div className="flex flex-row mt-4">
-                            <p className="flex align-center content-center text-base justify-start mr-2 text-slate-600 mr-6">
-                                <FaClock className="mt-1 mr-2 text-blue-500"/>{movie.runtime} min
-                            </p>
-                            <p className="flex align-center content-center text-base justify-start mr-2 text-slate-600 mr-4">
-                                <FaStar className="mt-1 mr-2 text-yellow-400"/>{movie.vote_average}
-                            </p>
-                        </div>
-                        <div className="flex flex-row mt-4">
-                            <p className="bg-slate-200 text-slate-800 py-1 px-2 rounded text-xs font-semibold mr-2">Drama</p>
-                            <p className="bg-slate-200 text-slate-800 py-1 px-2 rounded text-xs font-semibold mr-2">History</p>
-                        </div>
-                        <div className="mt-4">
-                            <p className="text-slate-400 font-light">{movie.overview}</p>
+                            <button onClick={addWatched} className='border border-slate-200 p-2 rounded'>
+                                <FaEye className="text-slate-400"/>
+                            </button>
                         </div>
                         <div className="my-4">
-                            <h3 className="font-serif text-base text-slate-700 font-bold">Directed by</h3>
-                            <p className="text-slate-400 font-light">Christopher Nolan</p>
+                            <h3 className="font-serif text-base text-slate-700 font-bold">Tagline</h3>
+                            <p className="text-slate-400 font-light">{movie.tagline}</p>
                         </div>
+                        <div className="mt-4">
+                            <h3 className="font-serif text-base text-slate-700 font-bold">Overview</h3>
+                            <p className="text-slate-400 font-light">{movie.overview}</p>
+                        </div>
+                        
                     </div>
                 </div>
                 <main>
@@ -71,6 +81,8 @@ function DetailsPage () {
                 </main>
             </Container>
         </div>
+        </>)};
+        </>
     )
 }
 
